fefffb864b7b87dd3fb2c501fcf48f4c390ea7dc
이상현 <lisanghyun@gmail.com>
2026-06-04 10:38:35 +0900

    [refactoring] Parser: 변수 선언 에러 메시지 및 표현식 우선순위 주석 추가
    consume()의 에러 메시지 명확화 및 표현식 파싱 우선순위 계층 주석 보강

--- src/assembler/parser.cpp
+++ src/assembler/parser.cpp
@@ -31,7 +31,9 @@ std::unique_ptr<Stmt> Parser::expressionStatement() {
     consume(TokenType::SEMICOLON, "Expect ';' after expression.");
     return std::make_unique<ExpressionStmt>(std::move(e));
 }
-std::unique_ptr<Expr> Parser::expression()  { return assignment(); }
+std::unique_ptr<Expr> Parser::expression() {
+    return assignment();  // assignment < logicalOr < ... < primary
+}
 std::unique_ptr<Expr> Parser::assignment()  {
     auto e = logicalOr();
     if (matchAny({TokenType::EQUAL})) {
